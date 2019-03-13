0ab5fd1ff63d2bd687df164ce23c773e2570b421
Jay C <jaykafkachen@gmail.com>
2019-03-12 19:07:43 -0700

    init

---
+++ fibonacci.asm
@@ -0,0 +1,19 @@
+# Who:  Me
+# What: project_template.asm
+# Why:  A template to be used for all CS264 labs
+# When: Created when? Due when?
+# How:  List the uses of registers
+
+.data
+
+
+.text
+.globl main
+
+
+main:	# program entry
+
+
+
+li $v0, 10		# terminate the program
+syscall
